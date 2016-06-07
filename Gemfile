0ad1d29a7834ed81b53e9f5f02ded83a76b73503
toby676 <tjcmui@gmail.com>
2016-06-07 09:55:29 +0100

    add picture and border to the aweseom web page

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 ruby '2.3.0'
 
 gem 'sinatra' 
+
+gem 'shotgun'
--- app.rb
+++ app.rb
@@ -3,3 +3,17 @@ require 'sinatra'
 get '/' do
   "Hello World"
 end
+
+get '/secret' do
+	"this is a secret - do not tell anyone"
+end
+
+get '/fish' do
+	'trout kipper goldfish'
+end
+
+get '/cat' do
+	"<div >
+	<img style='border: 3px dashed red' src= http://bit.ly/1eze8aE>
+	</div>"
+end
